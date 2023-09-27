8a40b27372aa747aee01476c2c065694951f191a
Felipe Zanetti <felipe.zanetti@codificar.com.br>
2023-09-27 13:10:30 -0300

    Fix navigation

--- navigation.js
+++ navigation.js
@@ -1,11 +1,9 @@
 //This file must be imported by the parent project that uses this model to function the navigation
-import React from 'react';
 import HelpChatScreen from './src/pages/HelpChatScreen';
 import RideChatScreen from './src/pages/RideChatScreen';
 import ListDirectsScreen from './src/pages/ListDirectsScreen';
 import DirectChatScreen from './src/pages/DirectChatScreen';
 import ListProvidersForConversation from './src/pages/ListProvidersForConversation';
-import { createStackNavigator } from '@react-navigation/stack';
 
 const screens = {
     HelpChatScreen: { screen: HelpChatScreen },
@@ -15,25 +13,6 @@ const screens = {
     ListProvidersForConversation: { screen: ListProvidersForConversation }
 };
 
-const Stack = createStackNavigator();
-
-const ChatStack = () => (
-  <Stack.Navigator
-    screenOptions={{
-      headerShown: false,
-    }}
-  >
-    <Stack.Screen name="DirectChatScreen" component={DirectChatScreen} />
-    <Stack.Screen name="HelpChatScreen" component={HelpChatScreen} />
-    <Stack.Screen name="ListDirectsScreen" component={ListDirectsScreen} />
-    <Stack.Screen
-      name="ListProvidersForConversation"
-      component={ListProvidersForConversation}
-    />
-    <Stack.Screen name="RideChatScreen" component={RideChatScreen} />
-  </Stack.Navigator>
-);
-
 export {
     HelpChatScreen,
     RideChatScreen,
@@ -42,4 +21,4 @@ export {
     ListProvidersForConversation,
 }
 
-export default ChatStack;
+export default screens;
